6e11df30b8fcbc224c0125521fcb089798f18a69
noufanelachola <noufansadiqelachola@gmail.com>
2024-07-11 00:47:36 +0530

    new

--- 01-binarySearch.py
+++ 01-binarySearch.py
@@ -14,6 +14,16 @@ tests = [
 ]
 
 
+def location(cards,number,mid):
+    midCard = cards[mid]
+    if midCard == number:
+        if mid-1 >= 0 and cards[mid-1] == midCard:
+            mid -= 1
+            return location(cards,number,mid)
+        else:
+            return mid
+
+    
 
 def locateCards(cards,number):
     low = 0
@@ -25,12 +35,12 @@ def locateCards(cards,number):
         print(f'low : {low} , high : {high} , mid : {mid} , midNo : {cards[mid]}')
         
         if cards[mid] == number :
-            return mid
+            return location(cards,number,mid)
         elif cards[mid] > number :
             low = mid + 1
         elif cards[mid] < number :
             high = mid - 1
     return -1
 
-result = locateCards([15, 10, 8, 6, 5, 2, 0],8)
+result = locateCards([10],10)
 print(result)

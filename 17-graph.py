5836830b156130defc9926e5ef61df074c90abc8
noufanelachola <noufansadiqelachola@gmail.com>
2025-03-17 06:24:00 +0530

    DFS implemented

--- 17-graph.py
+++ 17-graph.py
@@ -11,6 +11,36 @@ class Graph:
             self.graph[node1].append(node2)
             self.graph[node2].append(node1)
 
+    def bfs(self):
+        queue = [next(iter(self.graph))]  
+        visited = set()
+
+        while queue:
+            x = queue.pop(0)
+            if x not in visited:
+                print(x,end=" ")
+                visited.add(x)
+                queue.extend(self.graph[x])
+        print("\n")
+        
+
+    def dfs(self):
+        stack = [next(iter(self.graph))]  
+        visited = set()
+
+        while stack:
+            x = stack.pop()
+            if x not in visited:
+                visited.add(x)
+                print(x,end=" ")
+
+                for neighbour in self.graph[x]:
+                    if neighbour not in visited:
+                        stack.append(neighbour)
+                        # break
+        print("\n")
+
+
     def display(self):
         for node in self.graph:
             print(f"{node} -> {self.graph[node]}")
@@ -24,5 +54,7 @@ g.add_edge('A', 'B')
 g.add_edge('A', 'C')
 g.add_edge('B', 'C')
 g.add_edge('B', 'D')
+g.bfs()
+g.dfs()
 
 g.display()
